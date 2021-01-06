40af9dfc279fcc9b5bd8c53d0d404639e1f6c5b9
TrueDollar <truedollar@protonmail.com>
2021-01-06 16:55:25 +0700

    Use HDWalletProvider instead of PrivateKeyProvider

--- protocol/truffle.js
+++ protocol/truffle.js
@@ -18,7 +18,6 @@
  *
  */
 require('dotenv').config();
-const PrivateKeyProvider = require('truffle-privatekey-provider');
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const privateKey = process.env.TSD_PRIVATE_KEY;
 const infuraId = process.env.TSD_INFURA_ID;
@@ -62,7 +61,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
-      provider: () => new PrivateKeyProvider(privateKey, 'https://ropsten.infura.io/v3/' + infuraId),
+      provider: () => new HDWalletProvider(privateKey, 'https://ropsten.infura.io/v3/' + infuraId),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
@@ -113,6 +112,6 @@ module.exports = {
   ],
 
   api_keys: {
-    etherscan: etherscanKey
+    etherscan: etherscanKey,
   }
-}
+};
